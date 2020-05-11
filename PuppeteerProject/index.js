ef124bff39308c0d68e546add8fe50630c73e527
gauravxdhingra <grvdhingra1999@gmail.com>
2020-05-11 13:15:53 +0530

    Project Finished and Polished

--- PuppeteerProject/index.js
+++ PuppeteerProject/index.js
@@ -23,35 +23,20 @@ let course = process.argv[3];
         // tab
         let tabs = await browser.pages();
         let tab = tabs[0];
-        // dom => html 
-        //  browser=> 500ms request 
-        // hk login page
+
         // ***********************Login*************************
         await tab.goto(url, { waitUntil: "networkidle2" });
-        // await tab.click("button[data-purpose=header-login]");
 
         // let bodyHTML = await tab.evaluate(() => document.);
-        let bodyHTML = await tab.content();
-        console.log(bodyHTML)
+        // let bodyHTML = await tab.content();
+        // console.log(bodyHTML)
 
 
         await tab.waitForSelector("input[type=email]");
         await tab.type("input[type=email]", user, { delay: 120 });
-        // const googleLoginButtonSelector = 'body > section > ... > div'
-        // await tab.waitForSelector( googleLoginButtonSelector )
-        // await page.click( googleLoginButtonSelector )
 
         await tab.type("input[type=password]", pwd, { delay: 120 });
-        //  _1xnd => group of post 
-        // _4-u2 _4-u8=> particular post
-        //  inside ._1xnd
-        // descendent => select 
-        // 1xnd => last
-        // await Promise.all([
-        //     tab.click("input[data-purpose=do-login]"), tab.waitForNavigation({
-        //         waitUntil: "networkidle2"
-        //     })
-        // ]);
+
         await tab.click("input[data-purpose=do-login]")
 
         // ***********************Search Courses*************************
@@ -59,45 +44,9 @@ let course = process.argv[3];
         await tab.waitForSelector("input[data-purpose=search-input]");
         await tab.type("input[data-purpose=search-input]", course, { delay: 120 });
 
-        // await Promise(
-        //     tab.click("button[type=submit]"), tab.waitForNavigation({
-        //         waitUntil: "networkidle2"
-        //     })
-        // );
         await tab.click("button[type=submit]")
-
-        // await tab.goto(pUrl, { waitUntil: "networkidle2" });
-        // await tab.waitForSelector("div[data-key=tab_posts]");
-        // //  post => click => reroute=> 2 times=> 2 times (wait for navigation)
-        // await Promise.all([
-        //     tab.click("div[data-key=tab_posts]"),
-        //     tab.waitForNavigation({ waitUntil: "networkidle2" })
-        // ])
-        // await tab.waitForNavigation({ waitUntil: "networkidle2" });
         await handleSinglePageQuestion(tab, browser);
-        // let idx = 0;
-        // do {
-        //     //  post => 7 post => are loaded 
-        //     await tab.waitForSelector("#pagelet_timeline_main_column ._1xnd .clearfix.uiMorePager");
-        //     // children selector
 
-        //     let elements = await tab.$$("#pagelet_timeline_main_column ._1xnd > ._4-u2._4-u8")
-        //     // saftey
-        //     // console.log(elements.length);
-        //     let post = elements[idx];
-        //     // like -> selector
-        //     await tab.waitForSelector("._666k ._8c74");
-        //     let like = await post.$("._666k ._8c74");
-        //     await like.click({ delay: 100 });
-        //     idx++;
-        //     await tab.waitForSelector(".uiMorePagerLoader", { hidden: true })
-        //     //  wait for loader => visible => content load =>
-        //     // hidden=> may post => wait for loader 
-        //     //  loader  hide wait 
-        //     // immediate child
-        //     //  descendent 
-        // } while (idx < nPost)
-        // // await browser.close();
     } catch (err) {
         console.log(err)
     }
@@ -122,51 +71,41 @@ async function handleSinglePageQuestion(tab, browser) {
         pArr.push(mWillAddedPromisetocQ);
     }
     await Promise.all(pArr);
-    // // go to next page 
-    // await tab.waitForSelector(".pagination ul li");
-    // let paginationBtn = await tab.$$(".pagination ul li");
 
-    // let nxtBtn = paginationBtn[paginationBtn.length - 2];
-    // let className = await tab.evaluate(function (nxtBtn) {
-    //     return nxtBtn.getAttribute("class");
-    // }, nxtBtn);
-    // if (className === "disabled") {
-    //     return;
-    // } else {
-    //     await Promise.all([nxtBtn.click(), tab.waitForNavigation({ waitUntil: "networkidle0" })]);
-    //     await handleSinglePageQuestion(tab, browser);
-    // }
 }
 
 async function handleSingleQuestion(newTab, link, i) {
     let x = i + 1;
     await newTab.goto(link);
-    // await newTab.goto(link, { waitUntil: "networkidle2" });
-    //  popup => save changes may not have been saved 
-    await newTab.waitForSelector("button[data-purpose=toggle-wishlist]");
-    await newTab.click("button[data-purpose=toggle-wishlist]");
-    // await Promise.all([
-    //     newTab.click("li[data-tab=moderators]"),
-    //     newTab.waitForNavigation({ waitUntil: "networkidle0" })
-    // ])
-    // await newTab.waitForSelector("input[id=moderator]", { visible: true });
-    // await newTab.type("#moderator", "theamanthakur");
-    // await newTab.keyboard.press("Enter")
 
+    // await newTab.waitForSelector("button[data-purpose=toggle-wishlist]");
+    // await newTab.click("button[data-purpose=toggle-wishlist]");
 
-    // await newTab.waitForSelector(".wishlist--wishlist--2riVP.wishlist-button--style-inverse--22PW4.wishlist-button--wishlist-btn--3Xy6s.btn.btn-link");
-    // await newTab.click(".wishlist--wishlist--2riVP.wishlist-button--style-inverse--22PW4.wishlist-button--wishlist-btn--3Xy6s.btn.btn-link");
+    await newTab.waitForSelector(".wishlist--wishlist--2riVP.wishlist-button--style-inverse--22PW4.wishlist-button--wishlist-btn--3Xy6s.btn.btn-link");
+    await newTab.click(".wishlist--wishlist--2riVP.wishlist-button--style-inverse--22PW4.wishlist-button--wishlist-btn--3Xy6s.btn.btn-link");
     await newTab.waitFor(2000);
 
     const titl = await newTab.evaluate(() => document.querySelector(".clp-lead__title").textContent);
-    const title = titl.substring(1, titl.length - 1);
+    const title = titl.substring(1, titl.length - 1).replace(':', '');
     console.log(title);
 
+
+    const pric = await newTab.evaluate(() => document.querySelector("div[data-purpose=course-price-text]").textContent);
+    console.log(pric);
+
+    const desc = await newTab.evaluate(() => document.querySelector("div[data-purpose=collapse-description-text]").innerText);
+
+    const output = titl + pric + '\nDescription' + desc;
+
+    fs.writeFileSync('./Output/' + x + '. ' + title + '.txt', output);
+
     // await newTab.pdf({ path: './PDFOutput/' + x +'. '+ title+'.pdf' , format: 'A4' });
-    await newTab.screenshot({ path: './PDFOutput/' + x + ' ' + title + '.png', fullPage: true });
+
+    await newTab.waitForSelector(".wishlist--wishlist--2riVP.wishlist-button--style-inverse--22PW4.wishlist-button--wishlist-btn--3Xy6s.wishlist-button--active--2i1GY.btn.btn-link");
+    await newTab.screenshot({ path: './Output/' + x + ' ' + title + '.png', fullPage: true });
 
     // let bodyHTML = await newTab.evaluate(() => document.body.innerHTML);
-    let bodyHTML= await newTab.content();
+    // let bodyHTML = await newTab.content();
     // console.log(bodyHTML);
 
     // var options = { format: 'Letter' };
@@ -174,9 +113,7 @@ async function handleSingleQuestion(newTab, link, i) {
     //     if (err) return console.log(err);
     //     console.log(res); // { filename: '/app/businesscard.pdf' }
     // });
-   await fs.writeFileSync('./PDFOutput/' + x + '. ' + title + '.html', bodyHTML);
-
-    // await newTab.waitForSelector(".wishlist--wishlist--2riVP.wishlist-button--style-inverse--22PW4.wishlist-button--wishlist-btn--3Xy6s.wishlist-button--active--2i1GY.btn.btn-link");
+    // await fs.writeFileSync('./PDFOutput/' + x + '. ' + title + '.html', bodyHTML);
 
     await newTab.close();
 }
